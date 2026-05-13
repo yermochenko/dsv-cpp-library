78883e20d4a39df53a2cecded2aed1815832fd2a
Sergey Yermochenko <yermochenko@gmail.com>
2026-05-13 10:56:05 +0300

    Добавлены модульные тесты для проверки вывода таблиц с пустыми строками/ячейками

--- dsv-library-unit-tests/table-output.cpp
+++ dsv-library-unit-tests/table-output.cpp
@@ -11,7 +11,7 @@ namespace DsvLibraryUnitTests
 	TEST_CLASS(TableOutputUnitTests)
 	{
 	public:
-		TEST_METHOD(test1)
+		TEST_METHOD(test01)
 		{
 			Table table;
 			table.getData() = {
@@ -23,7 +23,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123\tabc\r\n456\txyz\r\n789\tmnk"), sout.str());
 		}
-		TEST_METHOD(test2)
+		TEST_METHOD(test02)
 		{
 			Table table(';');
 			table.getData() = {
@@ -35,7 +35,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123;abc\r\n456;xyz\r\n789;\"mnk; pqr; uvw\""), sout.str());
 		}
-		TEST_METHOD(test3)
+		TEST_METHOD(test03)
 		{
 			Table table('\t', '\'');
 			table.getData() = {
@@ -47,7 +47,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123\tabc\txyz\r\n456\r\n789\t'mnk, ''pqr'', uvw'"), sout.str());
 		}
-		TEST_METHOD(test4)
+		TEST_METHOD(test04)
 		{
 			Table table(',', '\'');
 			table.getData() = {
@@ -59,7 +59,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123,abc\r\n456,xyz\r\n789,'mnk, ''pqr'', uvw'"), sout.str());
 		}
-		TEST_METHOD(test5)
+		TEST_METHOD(test05)
 		{
 			Table table(';', '\'', Eol::LF);
 			table.getData() = {
@@ -71,7 +71,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123;abc\n456;xyz\n789;'mnk,\npqr,\nuvw'"), sout.str());
 		}
-		TEST_METHOD(test6)
+		TEST_METHOD(test06)
 		{
 			Table table(';', '"', Eol::CR);
 			table.getData() = {
@@ -83,7 +83,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123;abc\r456;xyz\r789;\"mnk;\rpqr;\ruvw\""), sout.str());
 		}
-		TEST_METHOD(test7)
+		TEST_METHOD(test07)
 		{
 			Table table('\t', '"', Eol::LF);
 			table.getData() = {
@@ -95,7 +95,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123\tabc\n456\txyz\n789\t\"mnk,\n\"\"pqr\"\",\nuvw\""), sout.str());
 		}
-		TEST_METHOD(test8)
+		TEST_METHOD(test08)
 		{
 			Table table('\t', '"', Eol::CRLF);
 			table.getData() = {
@@ -107,7 +107,7 @@ namespace DsvLibraryUnitTests
 			sout << table;
 			Assert::AreEqual(std::string("123\tabc\r\n456\txyz\r\n789\t\"mnk\t\"\"pqr\"\"\r\nuvw\""), sout.str());
 		}
-		TEST_METHOD(test9)
+		TEST_METHOD(test09)
 		{
 			Table table(';');
 			table.getData() = {
@@ -118,6 +118,31 @@ namespace DsvLibraryUnitTests
 			std::ostringstream sout;
 			sout << table;
 			Assert::AreEqual(std::string("123;abc\r\n456;xyz\r\n789;\"mnk;\n\"\"pqr\"\";\nuvw\""), sout.str());
+		}
+		TEST_METHOD(test10)
+		{
+			Table table(';');
+			table.getData() = {
+				{"123", "abc"},
+				{},
+				{"456", "xyz"},
+				{"789", "mnk"}
+			};
+			std::ostringstream sout;
+			sout << table;
+			Assert::AreEqual(std::string("123;abc\r\n\r\n456;xyz\r\n789;mnk"), sout.str());
+		}
+		TEST_METHOD(test11)
+		{
+			Table table(';');
+			table.getData() = {
+				{"123", "abc"},
+				{"456", "", "xyz"},
+				{"789", "mnk"}
+			};
+			std::ostringstream sout;
+			sout << table;
+			Assert::AreEqual(std::string("123;abc\r\n456;;xyz\r\n789;mnk"), sout.str());
 		}
 	};
 }
