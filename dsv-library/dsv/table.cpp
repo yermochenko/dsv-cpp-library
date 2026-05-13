82f77c3d4d3d3cdcb6c2c996967529d7d8e50a6c
Sergey Yermochenko <yermochenko@gmail.com>
2026-05-13 13:58:03 +0300

    Добавлен класс для представления ошибки парсинга

--- dsv-library/dsv/table.cpp
+++ dsv-library/dsv/table.cpp
@@ -16,3 +16,18 @@ std::ostream& operator<<(std::ostream& out, Table& table)
 	// TODO: реализовать парсинг DSV формата
 	return out;
 }
+
+const int ParseError::getRowIndex() const
+{
+	return rowIndex;
+}
+
+const int ParseError::getValueIndex() const
+{
+	return valueIndex;
+}
+
+const std::string ParseError::getMessage() const
+{
+	return message;
+}
--- dsv-library/dsv/table.h
+++ dsv-library/dsv/table.h
@@ -52,6 +52,44 @@ public:
 	std::vector<std::vector<std::string>>& getData();
 };
 
+/**
+Представляет собой ошибку, которая может возникнуть при разборе потока символов
+в формате DSV
+*/
+class ParseError
+{
+	const int rowIndex;
+	const int valueIndex;
+	const std::string message;
+public:
+	/**
+	Создаёт ошибку, возникшую при разборе (парсинге) входного потока (файла)
+	в формате DSV
+	\param[in] _rowIndex   Индекс строки во входном потоке, содержащей ошибочный
+	                       символ
+	\param[in] _valueIndex Индекс значения (ячейки) в строке, содержащего
+	                       ошибочный символ
+	\param[in] _message    Текстовое описание возникшей ошибки
+	*/
+	ParseError(
+		const int _rowIndex,
+		const int _valueIndex,
+		const std::string _message
+	) : rowIndex(_rowIndex),
+		valueIndex(_valueIndex),
+		message(_message) {}
+	const int getRowIndex() const;
+	const int getValueIndex() const;
+	const std::string getMessage() const;
+};
+
+/**
+Выполняет чтение данных из потока, разбор (парсинг) этих данных в формате DSV и
+сохранение данных в указанную таблицу (объект класса Table)
+\throw ParseError В случае, когда входной поток содержит некорректные данные не
+                  соответствующие формату DSV (в соответствии с натройками
+				  переданного объекта класса Table)
+*/
 std::istream& operator>>(std::istream&, Table&);
 
 std::ostream& operator<<(std::ostream&, Table&);
