f80ef7d08ae38c0878a0ac4535dbdc641ba837e7
Sergey Yermochenko <yermochenko@gmail.com>
2026-05-14 10:53:06 +0300

    Реализан вывод таблицы в DSV формат

--- dsv-library/demo.cpp
+++ dsv-library/demo.cpp
@@ -1,7 +1,116 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include "dsv/table.h"
+
+Table create()
+{
+	std::string line;
+	std::cout << "Enter delimeter of values in table row (enter one character and press 'Enter', or just press 'Enter' to use default value - tabulation): ";
+	std::getline(std::cin, line);
+	char delimeter;
+	if (line.empty())
+	{
+		delimeter = '\t';
+	}
+	else if (line.size() == 1)
+	{
+		delimeter = line[0];
+	}
+	else
+	{
+		throw std::string("You enter more than one character");
+	}
+	std::cout << "Enter enclosure of values in table row (enter one character and press 'Enter', or just press 'Enter' to use default value - double quote): ";
+	std::getline(std::cin, line);
+	char enclosure;
+	if (line.empty())
+	{
+		enclosure = '"';
+	}
+	else if (line.size() == 1)
+	{
+		enclosure = line[0];
+	}
+	else
+	{
+		throw std::string("You enter more than one character");
+	}
+	std::cout << "Choose delimeter of rows:" << std::endl;
+	std::cout << "  LF (code 10 or 0x0A or \\n)" << std::endl;
+	std::cout << "  CR (code 13 or 0x0D or \\r)" << std::endl;
+	std::cout << "  CRLF (two characters)" << std::endl;
+	std::cout << "Enter value LF, or CR, or CRLF, and press 'Enter', or just press 'Enter' to use devault value - CRLF: ";
+	std::getline(std::cin, line);
+	Eol eol;
+	if(line == "LF")
+	{
+		eol = Eol::LF;
+	}
+	else if (line == "CR")
+	{
+		eol = Eol::CR;
+	}
+	else if (line == "CRLF" || line.empty())
+	{
+		eol = Eol::CRLF;
+	}
+	else
+	{
+		throw std::string("You enter invalid value");
+	}
+	return Table(delimeter, enclosure, eol);
+}
+
+void write()
+{
+	std::cout << "Demonstration of writing table into file" << std::endl;
+	try
+	{
+		Table table = create();
+		std::cout << "Enter amount of rows in table: ";
+		std::string line;
+		std::getline(std::cin, line);
+		int n = std::stoi(line);
+		std::vector<std::vector<std::string>> data;
+		for (int i = 0; i < n; i++)
+		{
+			std::cout << "  Enter amount of values in row #" << (i + 1) << ": ";
+			std::getline(std::cin, line);
+			int k = std::stoi(line);
+			std::vector<std::string> row;
+			for (int j = 0; j < k; j++)
+			{
+				std::cout << "    Enter value #" << (j + 1) << ": ";
+				std::getline(std::cin, line);
+				row.push_back(line);
+			}
+			data.push_back(row);
+		}
+		table.getData() = data;
+		std::cout << "Enter path to file to store table: ";
+		std::getline(std::cin, line);
+		std::ofstream fout(line, std::ios::binary);
+		if (fout.is_open())
+		{
+			fout << table;
+		}
+		else
+		{
+			std::cout << "Can't write to file \"" << line << "\"" << std::endl;
+		}
+	}
+	catch (std::string message)
+	{
+		std::cout << message << std::endl;
+	}
+}
 
 int main()
 {
 	std::cout << "Demonstration of using DSV (Delimeter-Separated Values) library" << std::endl;
+	write();
 	return 0;
 }
--- dsv-library/dsv/table.cpp
+++ dsv-library/dsv/table.cpp
@@ -7,13 +7,55 @@ std::vector<std::vector<std::string>>& Table::getData()
 
 std::istream& operator>>(std::istream& in, Table& table)
 {
-	// TODO: реализовать вывод таблицы в DSV формате
+	// TODO: реализовать парсинг DSV формата
 	return in;
 }
 
 std::ostream& operator<<(std::ostream& out, Table& table)
 {
-	// TODO: реализовать парсинг DSV формата
+	std::vector<std::vector<std::string>> data = table.getData();
+	for (size_t i = 0, n = data.size(); i < n; i++)
+	{
+		for (size_t j = 0, k = data[i].size(); j < k; j++)
+		{
+			std::string value = data[i][j];
+			bool enclosed = false;
+			if (value.find(table.VALUE_DELIMETER) != std::string::npos ||
+			    value.find('\r') != std::string::npos ||
+				value.find('\n') != std::string::npos)
+			{
+				enclosed = true;
+			}
+			size_t pos = 0;
+			while ((pos = value.find(table.VALUE_ENCLOSURE, pos)) != std::string::npos)
+			{
+				enclosed = true;
+				value.insert(pos, 1, table.VALUE_ENCLOSURE);
+				pos += 2;
+			}
+			if (enclosed)
+			{
+				out << table.VALUE_ENCLOSURE << value << table.VALUE_ENCLOSURE;
+			}
+			else
+			{
+				out << value;
+			}
+			if (j < k - 1)
+			{
+				out << table.VALUE_DELIMETER;
+			}
+		}
+		if (i < n - 1)
+		{
+			switch (table.ROW_DELIMETER)
+			{
+			case Eol::LF:   out << '\n';   break;
+			case Eol::CR:   out << '\r';   break;
+			case Eol::CRLF: out << "\r\n"; break;
+			}
+		}
+	}
 	return out;
 }
 
